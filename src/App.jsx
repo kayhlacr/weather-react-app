28004107158475b1c0ccffb705586d731d2f4d38
Kayhlacr <kayhla@student.dev-island.org>
2024-03-28 18:42:49 -1000

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import WeatherForm from "./WeatherForm";
 import WeatherDisplay from "./WeatherDisplay";
 
-const apiKey = import.meta.env.VITE_API_KEY
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const API_ROOT = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&days=5&aqi=no&alerts=no`;
 
@@ -37,8 +37,8 @@ const App = () => {
   const fetchWeatherAsync = async () => {
     setLoading(true);
     try {
-      const res = await fetch(api)
-      const data = await res.json()
+      const res = await fetch(api);
+      const data = await res.json();
       setData(data);
     } catch (error) {
       console.error("Error fetching weather data:", error);
@@ -62,9 +62,7 @@ const App = () => {
         setUserLocation={setUserLocation}
         userLocation={userLocation}
       />
-      {loading ? <div>Loading...</div> : (
-        <WeatherDisplay data={data} />
-      )}
+      {loading ? <div>Loading...</div> : <WeatherDisplay data={data} />}
     </div>
   );
 };
